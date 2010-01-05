84b63d74a15528db490457ccd97da08997cad8e4
Roman Sklenar <mail@romansklenar.cz>
2010-01-05 02:47:40 +0100

    updated benchmark test & benchmark results

--- tests/libs/ActiveRecord/BenchmarkTest.php
+++ tests/libs/ActiveRecord/BenchmarkTest.php
@@ -15,6 +15,7 @@ class BenchmarkTest extends PHPUnit_Framework_TestCase {
 
 	public function testBenchmarkActiveRecord() {
 
+		Debug::$showLocation = FALSE;
 		$connection = new DibiConnection($this->config);
 		$connection->loadFile(APP_DIR . '/models/consumers.structure.sql');
 		$connection->loadFile(APP_DIR . '/models/consumers.data.sql');
@@ -22,13 +23,15 @@ class BenchmarkTest extends PHPUnit_Framework_TestCase {
 		CacheHelper::cleanCache();
 
 		// kalibrace
+		ini_set('memory_limit', '10M'); // memory_get_peak_usage() ~ 8.5M
 		dump('--- ActiveRecord ---');
-		$showLocation = FALSE;
-		$consumers = Consumer::find(1,2,3,4,5)->toArray();
+		Consumer::objects()->first();
+		Consumer::objects()->applyLimit(5)->load();
+		
 
 		timer('1');
 		memory('1');
-		$consumer = Consumer::findOne();
+		$consumer = Consumer::objects()->first();
 		$m = memory('1');
 		$t = timer('1');
 		dump(self::formatMemory($m));
@@ -36,7 +39,7 @@ class BenchmarkTest extends PHPUnit_Framework_TestCase {
 
 		timer('10');
 		memory('10');
-		$consumers = Consumer::find(1,2,3,4,5,6,7,8,9,10);
+		$consumers = Consumer::objects()->applyLimit(10);
 		$consumers->load();
 		$m = memory('10');
 		$t = timer('10');
@@ -45,27 +48,27 @@ class BenchmarkTest extends PHPUnit_Framework_TestCase {
 
 		timer('100');
 		memory('100');
-		$consumers = Consumer::find()->applyLimit(100);
+		$consumers = Consumer::objects()->applyLimit(100);
 		$consumers->load();
 		$m = memory('100');
 		$t = timer('100');
 		dump(self::formatMemory($m));
 		dump(self::formatTime($t));
 
+		ini_set('memory_limit', '12M');
 		timer('1000');
 		memory('1000');
-		$consumers = Consumer::find()->applyLimit(1000);
+		$consumers = Consumer::objects()->applyLimit(1000);
 		$consumers->load();
 		$m = memory('1000');
 		$t = timer('1000');
 		dump(self::formatMemory($m));
 		dump(self::formatTime($t));
 
-		$this->markTestIncomplete();
-
+		ini_set('memory_limit', '128M');
 		timer('50000');
 		memory('50000');
-		$consumers = Consumer::find();
+		$consumers = Consumer::objects()->applyLimit(50000);
 		$consumers->load();
 		$m = memory('50000');
 		$t = timer('50000');
@@ -77,17 +80,16 @@ class BenchmarkTest extends PHPUnit_Framework_TestCase {
 		CacheHelper::cleanCache();
 	}
 
-
 	public function testBenchmarkDibi() {
 
-		$showLocation = FALSE;
+		Debug::$showLocation = FALSE;
 		dibi::connect($this->config);
 		dibi::loadFile(APP_DIR . '/models/consumers.structure.sql');
 		dibi::loadFile(APP_DIR . '/models/consumers.data.sql');
 
 		// kalibrace
 		dump('--- Dibi ---');
-		$res = dibi::dataSource('Consumers')->applyLimit(1)->getResult()->detectTypes()->fetch();
+		$row = dibi::dataSource('Consumers')->applyLimit(1)->getResult()->detectTypes()->fetch();
 
 
 		timer('1');
@@ -122,13 +124,88 @@ class BenchmarkTest extends PHPUnit_Framework_TestCase {
 		dump(self::formatMemory($m));
 		dump(self::formatTime($t));
 
-		$this->markTestIncomplete();
-
 		timer('50000');
 		memory('50000');
 		$rows = dibi::dataSource('Consumers')->applyLimit(50000)->getResult()->detectTypes()->fetchAll();
 		$m = memory('50000');
 		$t = timer('50000');
+		dump(self::formatMemory($m));
+		dump(self::formatTime($t));
+
+		dibi::disconnect();
+	}
+
+	public function testGettingOneRecord() {
+
+		Debug::$showLocation = FALSE;
+		$connection = new DibiConnection($this->config);
+		$connection->loadFile(APP_DIR . '/models/consumers.structure.sql');
+		$connection->loadFile(APP_DIR . '/models/consumers.data.sql');
+		Mapper::addConnection($connection);
+		CacheHelper::cleanCache();
+
+		// kalibrace
+		dump('--- ActiveRecord: getting one record ---');
+		Consumer::find(1);
+		Consumer::findOne();
+		Consumer::objects()->first();
+		Consumer::objects()->applyLimit(1)->first();
+		
+
+		timer('1.1');
+		memory('1.1');
+		$consumer = Consumer::find(1);
+		$m = memory('1.1');
+		$t = timer('1.1');
+		dump(self::formatMemory($m));
+		dump(self::formatTime($t));
+
+		timer('1.2');
+		memory('1.2');
+		$consumer = Consumer::findOne();
+		$m = memory('1.2');
+		$t = timer('1.2');
+		dump(self::formatMemory($m));
+		dump(self::formatTime($t));
+
+		timer('1.3');
+		memory('1.3');
+		$consumer = Consumer::objects()->first();
+		$m = memory('1.3');
+		$t = timer('1.3');
+		dump(self::formatMemory($m));
+		dump(self::formatTime($t));
+
+		timer('1.4');
+		memory('1.4');
+		$consumer = Consumer::objects()->applyLimit(1)->first();
+		$m = memory('1.4');
+		$t = timer('1.4');
+		dump(self::formatMemory($m));
+		dump(self::formatTime($t));
+
+		// clean up
+		Mapper::disconnect();
+		CacheHelper::cleanCache();
+	}
+
+	public function testGettingOneRow() {
+
+		Debug::$showLocation = FALSE;
+		dibi::connect($this->config);
+		dibi::loadFile(APP_DIR . '/models/consumers.structure.sql');
+		dibi::loadFile(APP_DIR . '/models/consumers.data.sql');
+
+		// kalibrace
+		dump('--- Dibi: getting one row ---');
+		dibi::dataSource('Consumers')->applyLimit(1)->getResult()->detectTypes()->fetch();
+
+
+		timer('1');
+		memory('1');
+		$row = dibi::dataSource('Consumers')->applyLimit(1)->getResult()->detectTypes()->fetch();
+		$m = memory('1');
+		$t = timer('1');
 		dump(self::formatMemory($m));
 		dump(self::formatTime($t));
 
