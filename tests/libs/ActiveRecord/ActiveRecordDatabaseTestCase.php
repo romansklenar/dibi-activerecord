b5d97992742eb3ae9f26cb655df866c84efd7b92
Roman Sklenar <mail@romansklenar.cz>
2010-01-09 23:42:53 +0100

    ActiveRecordDatabaseTestCase: added relational models connections

--- tests/libs/ActiveRecord/ActiveRecordDatabaseTestCase.php
+++ tests/libs/ActiveRecord/ActiveRecordDatabaseTestCase.php
@@ -26,13 +26,21 @@ abstract class ActiveRecordDatabaseTestCase extends PHPUnit_Framework_TestCase {
 		Mapper::addConnection($connection, '#authors');
 
 		$connection = new DibiConnection($this->config);
+		$connection->loadFile(APP_DIR . '/models/one-to-one/o2o_nette.structure.sql');
+		$connection->loadFile(APP_DIR . '/models/one-to-one/o2o_nette.data.sql');
+		$connection->loadFile(APP_DIR . '/models/many-to-one/m2o_nette.structure.sql');
+		$connection->loadFile(APP_DIR . '/models/many-to-one/m2o_nette.data.sql');
 		$connection->loadFile(APP_DIR . '/models/many-to-many/m2m_nette.structure.sql');
-		//$connection->loadFile(APP_DIR . '/models/many-to-many/m2m_nette.data.sql');
+		$connection->loadFile(APP_DIR . '/models/many-to-many/m2m_nette.data.sql');
 		Mapper::addConnection($connection, '#nette_style');
 
 		$connection = new DibiConnection($this->config);
+		$connection->loadFile(APP_DIR . '/models/one-to-one/o2o_rails.structure.sql');
+		$connection->loadFile(APP_DIR . '/models/one-to-one/o2o_rails.data.sql');
+		$connection->loadFile(APP_DIR . '/models/many-to-one/m2o_rails.structure.sql');
+		$connection->loadFile(APP_DIR . '/models/many-to-one/m2o_rails.data.sql');
 		$connection->loadFile(APP_DIR . '/models/many-to-many/m2m_rails.structure.sql');
-		//$connection->loadFile(APP_DIR . '/models/many-to-many/m2m_rails.data.sql');
+		$connection->loadFile(APP_DIR . '/models/many-to-many/m2m_rails.data.sql');
 		Mapper::addConnection($connection, '#rails_style');
 
 		CacheHelper::cleanCache();
