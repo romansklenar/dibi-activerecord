b296fe582b125751db6b6aa4a8b6e912b1bfb136
Roman Sklenar <mail@romansklenar.cz>
2010-01-09 02:45:52 +0100

    birt models: added annotations and setted foreign masks

--- app/models/Order.php
+++ app/models/Order.php
@@ -3,7 +3,11 @@
 
 /**
  * Order active record model
+ *
+ * @belongsTo(Customer, OrderDetail)
+ * @hasMany(:OrderDetails => Products)
  */
 class Order extends ActiveRecord {
-	
+
+	protected static $foreingMask = '%primary%';
 }
